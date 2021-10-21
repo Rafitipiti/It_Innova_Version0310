8e887550e5e3db6ba2efb6110b7b8c29db365482
Setsuke <sergio_alva99@hotmail.com>
2021-10-20 21:04:45 -0500

    send email ready

--- src/main/java/pe/edu/upc/Tp2V2Application.java
+++ src/main/java/pe/edu/upc/Tp2V2Application.java
@@ -1,19 +1,28 @@
 package pe.edu.upc;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.context.event.EventListener;
 import pe.edu.upc.algorithm.algorithmService;
 import pe.edu.upc.algorithm.algorithmServiceFactory;
+import pe.edu.upc.service.EmailSenderService;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 import java.util.Arrays;
 
 @SpringBootApplication
 public class Tp2V2Application { // extends SpringBootServletInitializer
+
+    @Autowired //mail
+    private EmailSenderService service; //mail
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tp2V2Application.class, args);
@@ -31,5 +40,12 @@ public class Tp2V2Application { // extends SpringBootServletInitializer
     @Bean(name = "algorithmServicePython")
     public algorithmService algorithmServicePython() throws Exception {
         return algorithmFactory().getObject();
+    }
+
+    @EventListener(ApplicationReadyEvent.class) //mail
+    public void triggerMail(){ //mail
+        int randomNum = ThreadLocalRandom.current().nextInt(11111, 99999 + 1);
+
+        service.sendSimpleEmail("it.innova.service@gmail.com",String.valueOf(randomNum),"Reset Password"); //mail
     }
 }
---
+++ src/main/java/pe/edu/upc/service/EmailSenderService.java
@@ -0,0 +1,25 @@
+package pe.edu.upc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String toEmail, String body, String subject){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("it.innova.service@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+        System.out.println("mail send...");
+    }
+}
